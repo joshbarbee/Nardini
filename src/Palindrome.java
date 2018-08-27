d02258cd5a67b66413568067ddb1646edb22e880
Barbee <BarbeeJ10937@stxavier.org>
2018-08-27 10:55:47 -0400

    second

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,31 +1,38 @@
-import java.awt.List;
-import java.util.ArrayList;
-
 public class Palindrome {
-	public static String isPal(String s)
+	public static boolean isPal(String s)
 	{
 		String isPal = new StringBuilder(s).reverse().toString();
+		for(int x = 0; x<isPal.length(); x++) {
+			if (Character.isLetter(isPal.charAt(x)) == false) {
+				return false;}}
 		if(isPal.equals(s)) {
-			return "true";
+			return true;
 		}
-		return "false";
-			}
-	private static String isLetter(String letter) {
-		ArrayList<String> compLetters = new ArrayList<String>();
-		if (Character.isLetter(letter.charAt(0))) {
-			compLetters.add(letter.charAt(0));
-		}
-		return compLetters.toString();	
-	}
-	
-	private static String purge(String s) {
-		isLetter(letter);
-		return s;
-		
-	}
-	
-	public static boolean almostPal(String s) {
 		return false;
+			}
+	private static boolean isLetter(String letter) {
+		almostPal(letter);
+		if (Character.isLetter(letter.charAt(0))) {
+			return true;}
+		return false;
+	}
+	private static String purge(String x) {
+		isLetter(x);
+		String compLetters = "";
+		if(isLetter(x) == true) {
+			compLetters+=isLetter(x);
+			}
+		return x;}
+	
+	public static String almostPal(String s) 
+	{	
+		isPal(s);
+		String len = purge(s);
+			String alPal = new StringBuilder(purge(s)).reverse().toString();
+			if(alPal == purge(len)) {
+				return len;
+		}
+		return len;
 		
 	}
 }

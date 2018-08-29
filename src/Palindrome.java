d821821268815eca028d953c0593f937123413ba
Barbee <BarbeeJ10937@stxavier.org>
2018-08-29 08:28:04 -0400

    Fixed AlmostPal, included regex to speed up process and fix stack overflow

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,37 +2,38 @@ public class Palindrome {
 	public static boolean isPal(String s)
 	{
 		String isPal = new StringBuilder(s).reverse().toString();
+		//reverses string
 		for(int x = 0; x<isPal.length(); x++) {
 			if (Character.isLetter(isPal.charAt(x)) == false) {
+				//checks to see if the characters are all letters or not
 				return false;}}
 		if(isPal.equals(s)) {
+			//equality check between passed string and isPal string
 			return true;
 		}
+		//returns false if equality check !=
 		return false;
 			}
 	private static boolean isLetter(String letter) {
-		almostPal(letter);
 		if (Character.isLetter(letter.charAt(0))) {
 			return true;}
 		return false;
 	}
 	private static String purge(String x) {
-		isLetter(x);
-		String compLetters = "";
-		if(isLetter(x) == true) {
-			compLetters+=isLetter(x);
-			}
-		return x;}
+		//parses out all non-alpha inputs
+		return x.replaceAll("[^A-Za-z0-9]", "");}
 	
-	public static String almostPal(String s) 
+	public static boolean almostPal(String s) 
 	{	
-		isPal(s);
-		String len = purge(s);
-			String alPal = new StringBuilder(purge(s)).reverse().toString();
-			if(alPal == purge(len)) {
-				return len;
+		//calls the purged string method to parse out the string
+		String purgedString = purge(s);
+		//reverses the string 
+		String alPal = new StringBuilder(purgedString).reverse().toString();
+		if(alPal.equals(purgedString) && isPal(s) != true) {
+			//strict equality check
+			return true;
 		}
-		return len;
+		return false;
 		
 	}
 }
